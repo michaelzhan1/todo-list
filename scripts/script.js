e67c96ba767d9be23bd1c438b49a278a1e721ca0
Michael Zhan <michaelzhan2001@gmail.com>
2023-06-29 10:22:19 -0500

    Fix rendering for HTML characters in entered values

--- scripts/script.js
+++ scripts/script.js
@@ -42,32 +42,59 @@ document.addEventListener("DOMContentLoaded", function() {
     } else if (action === "delete") {
       var phrase = "Task successfully deleted!";
     }
-    toastContainer.innerHTML += `
-      <div class="toast hide bg-green" role="alert">
-        <div class="d-flex">
-          <div class="toast-body">
-            ${phrase}
-          </div>
-          <button type="button" class="btn-close me-2 m-auto" data-bs-dismiss="toast" aria-label="Close"></button>
-        </div>
-      </div>
-    `
-    return new bootstrap.Toast(document.querySelector('.toast:last-child'), {
+    let newToast = document.createElement("div");
+    newToast.classList.add("toast", "hide", "bg-green");
+    newToast.setAttribute("role", "alert");
+
+    let flexDiv = document.createElement("div");
+    flexDiv.classList.add("d-flex");
+
+    let toastBody = document.createElement("div");
+    toastBody.classList.add("toast-body");
+    toastBody.textContent = phrase;
+
+    let closeButton = document.createElement("button");
+    closeButton.classList.add("btn-close", "me-2", "m-auto");
+    closeButton.setAttribute("type", "button");
+    closeButton.setAttribute("data-bs-dismiss", "toast");
+
+    flexDiv.appendChild(toastBody);
+    flexDiv.appendChild(closeButton);
+    newToast.appendChild(flexDiv);
+    toastContainer.appendChild(newToast);
+
+    return new bootstrap.Toast(newToast, {
+      animation: true,
       autohide: true,
       delay: toastLifespan
-    });
+    })
   }
 
   // Add a new task
   function addTask(value) {
     let newId = `task${taskCount++}`;
-    taskList.innerHTML += `
-      <li class="list-group-item">
-        <input class="form-check-input me-1" type="checkbox" id="${newId}" name="${newId}" value="${newId}">
-        <label for="${newId}">${value}</label>
-        <button type="button" class="btn btn-danger show-delete-modal">Delete</button>
-      </li>
-    `
+    let listItem = document.createElement("li");
+    listItem.classList.add("list-group-item");
+
+    let checkbox = document.createElement("input");
+    checkbox.classList.add("form-check-input", "me-1");
+    checkbox.setAttribute("type", "checkbox");
+    checkbox.setAttribute("id", newId);
+
+    let label = document.createElement("label");
+    label.setAttribute("for", newId);
+    label.innerHTML = escapeHtml(value);
+    console.log(label.textContent)
+
+    let button = document.createElement("button");
+    button.classList.add("btn", "btn-danger", "show-delete-modal");
+    button.setAttribute("type", "button");
+    button.textContent = "Delete";
+
+    listItem.appendChild(checkbox);
+    listItem.appendChild(label);
+    listItem.appendChild(button);
+    taskList.appendChild(listItem);
   }
 
   // Check if task list is empty and show/hide placement text
@@ -77,6 +104,26 @@ document.addEventListener("DOMContentLoaded", function() {
     } else {
       emptyTaskListMessage.classList.remove("d-none");
     }
+  }
+
+  // Replace problematic characters with HTML entities
+  function escapeHtml(input) {
+    return input.replace(/[&<>"']/g, function(match) {
+      switch (match) {
+        case '&':
+          return '&amp;';
+        case '<':
+          return '&lt;';
+        case '>':
+          return '&gt;';
+        case "'":
+          return '&#x27;';
+        case '"':
+          return '&quot;';
+        default:
+          return match;
+      }
+    });
   }
   // ------- End of functions -------
 
